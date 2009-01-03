879499d4a63d07f6488d738c219306233a14b9ff
Lau_of_DK <lau.jensen@bestinclass.dk>
2009-01-03 23:12:22 +0100

    Modified demo.clj, now working.

--- src/dk/bestinclass/clojureql/demo.clj
+++ src/dk/bestinclass/clojureql/demo.clj
@@ -1,5 +1,6 @@
 ;; TEST ====================================================
 
+
 (ns dk.bestinclass.clojureql.demo
   (:gen-class)
   (:require
@@ -9,12 +10,15 @@
 ;
 ; MySQL:
 ;
-; CREATE TABLE employees (
-;   id int NOT NULL AUTO_INCREMENT,
-;   name varchar(100)  NOT NULL,
-;   language varchar(100)  NOT NULL,
-;   effeciency DOUBLE  NOT NULL,
-;   PRIMARY KEY (`id`));
+; CREATE TABLE `employees` (
+;  `id` int(11) NOT NULL auto_increment,
+;  `name` varchar(100) NOT NULL,
+;  `language` varchar(100) NOT NULL,
+;  `efficiency` double NOT NULL,
+;  `iq` int(11) NOT NULL,
+;  PRIMARY KEY  (`id`)
+;  ) ENGINE=MyISAM AUTO_INCREMENT=27 DEFAULT CHARSET=latin1
+;
 ;
 ; SQLite:
 ;
@@ -23,61 +27,46 @@
 ; Sorry. create-table is not finished, yet.
 
 ; Adapt the following connection-info to your local database.
-(def *conn-info*
-  {:jdbc-url "jdbc:sqlite:ClojureQL_Demo.db" :username "" :password ""})
+(def *conn-info* (sql/connect-info "mysql"
+                                   "localhost/mysql"
+                                   "mysqluser"
+                                   "mysqlpsw"))
+
 
 ; Set the correct driver here.
-(def *driver* "org.sqlite.JDBC")
+;(def *driver* "org.sqlite.JDBC")
+(def *driver* "com.mysql.jdbc.Driver")
 
 ; Then simply run this namespace to execute some example queries.
 
 (defn insert-data
   []
-  (let [columns [ :id :name     :language   :efficiency ]
-        data    [[1   "Frank"   "Python"    0.75       ]
-                 [2   "Brian"   "OCaml"     0.9        ]
-                 [3   "John"    "Fortran"   0.1        ]
-                 [4   "Mark"    "PHP"       0.71       ]
-                 [5   "Peter"   "SBCL"      0.9        ]
-                 [6   "Jack D." "Haskell"   0.9        ]
-                 [7   "Mike"    "C++"       0.002      ]
-                 [8   "Vader"   "Pure Evil" 0.99       ]
-                 [9   "Arnold"  "Cobol"     0.24       ]
-                 [10  "Chouser" "Clojure"   1          ]]
-        make-stmt (fn [id nom language efficiency]
-                    (sql/insert-into employees
-                                     id         ~id
+  (let [columns [    :name     :language   :efficiency   :iq ]
+        data    [[   "Frank"   "Python"    0.75           82 ]
+                 [   "Brian"   "OCaml"     0.9           119 ]
+                 [   "John"    "Fortran"   0.1           122 ]
+                 [   "Mark"    "PHP"       0.71          104 ]
+                 [   "Peter"   "SBCL"      0.9           123 ]
+                 [   "Jack D." "Haskell"   0.9           118 ]
+                 [   "Mike"    "C++"       0.002         107 ]
+                 [   "Vader"   "Pure Evil" 0.99          158 ]
+                 [   "Arnold"  "Cobol"     0.24           24 ]
+                 [   "Chouser" "Clojure"   1             192]]
+        make-stmt (fn [nom language efficiency iq]
+                    (sql/insert-into roster.employees
                                      name       ~nom
                                      language   ~language
-                                     efficiency ~efficiency))]
+                                     efficiency ~efficiency
+                                     iq          ~iq))]
     (doseq [stmt (map #(apply make-stmt %) data)]
       (sql/run *conn-info* stmt))))
 
 (defn -main
   [& args]
   (sql/load-driver *driver*)
-  (insert-data))
+;  (insert-data)
+  (sql/run [*conn-info* results]
+           (sql/query [id name language efficiency] roster.employees (> efficiency 0.5))
+           (doseq [row results]
+             (println row))))
 
-(comment "
-dk.bestinclass.clojureql> (execute
-                           (sql
-                            (query [id name] developers.employees)))
-{:name Frank, :id 1}
-
-{:name Brian, :id 2}
-{:name John, :id 3}
-{:name Mark, :id 4}
-{:name Peter, :id 5}
-{:name Jack D., :id 6}
-{:name Mike, :id 7}
-{:name Vader, :id 8}
-{:name Arnold, :id 9}
-{:name Chouser, :id 10}
-dk.bestinclass.clojureql> (execute
-                           (sql
-                            (query [id name] developers.employees
-                                   (and (> id 5)
-                                        (< id 8)))))
-{:name Jack D., :id 6}
-{:name Mike, :id 7}
-")
